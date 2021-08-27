7a8052f6d448d1d6e57f36a25037cf7f84419a3e
Collin <collinhallett@gmail.com>
2021-08-27 17:54:03 -0500

    a

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+}
+
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
   let filteredResults = animalsArray;
