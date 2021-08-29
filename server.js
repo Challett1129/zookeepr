33bdae8ca9d6fefbb3ed745313fbbcbdac4d4e0e
Collin <collinhallett@gmail.com>
2021-08-29 14:31:15 -0500

    added public folder and post functionality

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
 //parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+
 //parse incoming JSON data
 app.use(express.json());
 
-// if(process.env.NODE_ENV === 'production'){
-//   //set static folder
-//   app.use(express.static('client/build'));
-// }
+//middleware to allow server access to all files in public 
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -92,11 +92,17 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
+
 app.post('/api/animals', (req, res) => {
   //req.body is where our incoming content will be
   req.body.id = animals.length.toString();
   //if any data in req.body is incorrect, send 400 error back
-
+  
   if(!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -104,6 +110,7 @@ app.post('/api/animals', (req, res) => {
     res.json(req.body);
   }
 });
+
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
